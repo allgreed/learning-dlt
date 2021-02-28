e38c31a581335abd2b2f7db034c8c0c96c949cb9
Olgierd "Allgreed" Kasprowicz <olgierd@kasprowicz.pro>
2021-02-28 18:36:23 +0100

    Wrap it up with 1

--- src/main.py
+++ src/main.py
@@ -1,32 +1,54 @@
 import socket
 import asyncio
-import os, random
+import os
+import random
+import functools
 
-# inspired by:
-# https://stackoverflow.com/questions/46932654/udp-server-with-asyncio
-
-PORT = 5555
-# send messages via: nc -u localhost 5555
 
 def main():
-    print("Howdy!")
+    host = "127.0.0.1"
+    port = int(os.environ["APP_PORT"])
+    nodes_candidates = [
+        (host, 5555),
+        (host, 5556),
+        (host, 5557),
+    ]
+    nodes = [n for n in nodes_candidates if n[0] != host or n[1] != port]
+
+    print(f"Starting node at {host}:{port}, other nodes are: {nodes}")
+
     loop = asyncio.get_event_loop()
-    t = loop.create_datagram_endpoint(SyslogProtocol, local_addr=('localhost', PORT))
-    loop.run_until_complete(t) # Server starts listening
+
+    t = loop.create_datagram_endpoint(CustomProtocol, local_addr=(host, port))
+    loop.run_until_complete(t)
+
+    loop.create_task(perpetually(loop, functools.partial(send_udp_message_to_all, nodes=nodes, message="test")))
+
     loop.run_forever()
     
 
-class SyslogProtocol(asyncio.DatagramProtocol):
-    def __init__(self):
-        super().__init__()
-
-    def connection_made(self, transport) -> "Used by asyncio":
+class CustomProtocol(asyncio.DatagramProtocol):
+    def connection_made(self, transport):
         self.transport = transport
 
-    def datagram_received(self, data, addr) -> "Main entrypoint for processing message":
-        # Here is where you would push message to whatever methods/classes you want.
-        print(f"Received Syslog message: {data}")
+    def datagram_received(self, data, addr):
+        ip, port, *_ = addr
+        print(f"Message [{data.decode()}] from [{ip}]:[{port}]")
 
+
+def send_udp_message_to_all(nodes, message):
+    for n in nodes:
+        send_upd_message(*n, message)
+
+def send_upd_message(host, port, message) -> None:
+    sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+    sock.sendto(message.encode(), (host, port))
+
+
+async def perpetually(event_loop, f):
+    await asyncio.sleep(random.randint(500, 5000) / 1000)
+    f()
+    event_loop.create_task(perpetually(event_loop, f))
 
 if __name__ == "__main__":
     main()
